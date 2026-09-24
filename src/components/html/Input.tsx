57273968f35e0650bf632d12bf046fd1fce7850a
dojang102 <limij206@gmail.com>
2026-09-24 13:32:48 +0900

    todolist

---
+++ src/components/html/Input.tsx
@@ -0,0 +1,8 @@
+type Inputprops = Omit<React.ComponentPropsWithRef<"input">, "type"> & {
+    type?: "text";
+};
+
+export default function Input(props: Inputprops) {
+    const { ...rest } = props;
+    return <input {...rest} />;
+}
